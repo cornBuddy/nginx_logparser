f3e1fdc0309cb76f0dd1f0a775015c410d01e00e
cornBuddy <heorhi.sakovich@gmail.com>
2016-09-26 15:25:06 +0300

    css imported

--- usr/share/nginx_logparser/www/src/index.js
+++ usr/share/nginx_logparser/www/src/index.js
@@ -1,3 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import '../static/css/index.css'
+
 import Model from './helpers/model';
 import { initContext, drawDiagram, showError } from './helpers/viewLogic';
 const API = '';
