9bfb76da7326b4c0cac2b158b16f455a86847dec
cornBuddy <heorhi.sakovich@gmail.com>
2016-10-01 00:00:50 +0300

    server fixed

--- usr/share/nginx_logparser/www/devServer.js
+++ usr/share/nginx_logparser/www/devServer.js
@@ -1,4 +1,5 @@
 import http from 'http';
+import url from 'url';
 import fixture from './test/fixture'
 
 export default function runServer() {
@@ -14,10 +15,14 @@ export default function runServer() {
         const headers = {
           'Content-Type': 'application/json',
           'Access-Control-Allow-Origin': 'http://localhost:3000',
-        }
+        };
+        const paramObj = url.parse(request.url, true);
+        const since = Number(paramObj.query.timestamp);
+        const mock = Object.create(fixture);
+        mock._data = mock._data.filter(c => c.date >= since);
         response.on('error', throwError);
         response.writeHead(200, headers);
-        response.end(JSON.stringify(fixture));
+        response.end(JSON.stringify(mock));
       });
   }).listen(3333);
 }
