e2003389464ab60f3017ad90036350b883ec59b7
cornBuddy <heorhi.sakovich@gmail.com>
2016-10-01 09:56:12 +0300

    some logic added to entry point

--- usr/share/nginx_logparser/www/src/index.js
+++ usr/share/nginx_logparser/www/src/index.js
@@ -3,8 +3,10 @@ import 'bootstrap/dist/css/bootstrap-theme.css';
 import '../static/css/index.css'
 
 import Model from './helpers/model';
-import { initContext, drawDiagram, showError } from './helpers/viewLogic';
+import { initContext, drawDiagram, showError, aggregateBy }
+  from './helpers/viewLogic';
 const NODE_ENV = process.env.NODE_ENV || 'dev';
+const DAY = 1000 * 60 * 60 * 24;
 const API = NODE_ENV === 'dev'
   ? 'http://localhost:3333'
   : 'http://139.59.137.236/api/statistic';
@@ -13,5 +15,6 @@ const API = NODE_ENV === 'dev'
 const canvas = initContext();
 const mainLogModel = new Model(API);
 mainLogModel.data({ timestamp: 1474019000 })
+  .then(aggregateBy('date', { period: DAY }))
   .then(drawDiagram(canvas))
   .catch(showError);
